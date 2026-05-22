b981b4bccafeea68ccc677f44929a910fd83f8eb
sxnti328 <santiago.huertasb@uqvirtual.edu.co>
2026-05-21 22:19:57 -0500

    Atributos y metodos de algunas clases
    Persona, empleado, revision tecnica y gestorReporte

--- src/main/java/org/example/proyectofinalcodigo/model/clasesAbstractas/Empleado.java
+++ src/main/java/org/example/proyectofinalcodigo/model/clasesAbstractas/Empleado.java
@@ -1,4 +1,24 @@
 package org.example.proyectofinalcodigo.model.clasesAbstractas;
 
-public class Empleado {
-}
+public abstract class Empleado extends Persona {
+
+        private String idEmpleado;
+
+        public Empleado(String nombre, String documento, int edad, String idEmpleado) {
+            super(nombre, documento, edad);
+            this.idEmpleado = idEmpleado;
+        }
+
+        public String getIdEmpleado() {
+            return idEmpleado;
+        }
+        public void setIdEmpleado(String idEmpleado) {
+            this.idEmpleado = idEmpleado;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "  ID Empleado: " + idEmpleado;
+        }
+    }
+
